386df9b88db243aaedbafa60b362dd19cf71cb73
Hachem Sfar <35065444+hachemsfar@users.noreply.github.com>
2023-03-05 12:20:42 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TextBooster',
+    version='0.1',
+    description='Data augmentation techniques for text data using back translation and synonym replacement',
+    url='https://github.com/hachemsfar/TextBooster',
+    author='Your Name',
+    author_email='hachem.sfar@supcom.tn',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='text data augmentation back translation synonym replacement',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'deep_translator',
+        'nltk',
+        'spacy',
+        'textblob',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+        ]
+    },
+    project_urls={
+        'Bug Reports': 'https://github.com/hachemsfar/TextBooster/issues',
+        'Source': 'https://github.com/hachemsfar/TextBooster',
+    },
+)
